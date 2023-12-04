0c66329d1db3c46635b3887285144a3f4c047221
Zekun Zhang <zzekunn@gmail.com>
2023-12-04 14:15:49 -0500

    add form validation

--- components/CheckoutForm.tsx
+++ components/CheckoutForm.tsx
@@ -1,12 +1,24 @@
-import React from "react";
+import { useForm } from "react-hook-form";
 
-const CheckoutForm = ({ onClick }: any) => {
+const CheckoutForm = ({ onClick }: { onClick: () => void }) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const submitForm = (data: any) => {
+    if (data) {
+      onClick();
+    }
+  };
+
   return (
     <div className="max-w-md mx-auto mt-10 bg-white rounded-md shadow-md overflow-hidden">
       <div className="px-6 py-4 bg-gray-900 text-white">
         <h1 className="text-lg font-bold">Credit Card</h1>
       </div>
-      <form>
+      <form onSubmit={handleSubmit(submitForm)}>
         <div className="px-6 py-4">
           <div className="mb-4">
             <label
@@ -20,8 +32,13 @@ const CheckoutForm = ({ onClick }: any) => {
               id="card-number"
               type="text"
               placeholder="**** **** **** ****"
-              required
+              {...register("cardNumber", { required: true })}
             />
+            {errors.cardNumber && (
+              <p className="font-bold text-sm text-red-700 mt-1">
+                Card number is required.
+              </p>
+            )}
           </div>
 
           <div className="mb-4">
@@ -36,8 +53,13 @@ const CheckoutForm = ({ onClick }: any) => {
               id="expiration-date"
               type="text"
               placeholder="MM/YY"
-              required
+              {...register("expiration", { required: true })}
             />
+            {errors.expiration && (
+              <p className="font-bold text-sm text-red-700 mt-1">
+                Expiration date is required.
+              </p>
+            )}
           </div>
 
           <div className="mb-4">
@@ -49,8 +71,13 @@ const CheckoutForm = ({ onClick }: any) => {
               id="cvv"
               type="text"
               placeholder="***"
-              required
+              {...register("cvv", { required: true })}
             />
+            {errors.cvv && (
+              <p className="font-bold text-sm text-red-700 mt-1">
+                CVV is required.
+              </p>
+            )}
           </div>
 
           <div className="mb-4">
@@ -61,12 +88,17 @@ const CheckoutForm = ({ onClick }: any) => {
               className="appearance-none border border-gray-400 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
               type="text"
               placeholder="Full Name"
-              required
+              {...register("fullName", { required: true })}
             />
+            {errors.fullName && (
+              <p className="font-bold text-sm text-red-700 mt-1">
+                Full name is required.
+              </p>
+            )}
           </div>
 
           <button
-            onClick={onClick}
+            type="submit"
             className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-full"
           >
             Pay Now
